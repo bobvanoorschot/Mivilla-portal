f1ca7209d8195660c78c72f82aa49a1fed8858d6
bobvanoorschot <bobvanoorschot@gmail.com>
2019-11-18 11:26:49 +0100

    Customize result page for smaller websites

--- src/components/App.js
+++ src/components/App.js
@@ -1,94 +1,120 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import Loading from './icons/loading.svg';
-import 'react-dates/initialize';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Query } from "react-apollo";
+import Loading from "./icons/loading.svg";
+import "react-dates/initialize";
 
-import { PORTAL_QUERY } from '../_lib/queries';
+import { PORTAL_QUERY } from "../_lib/queries";
 
-import SearchPage from './SearchPage/SearchPage';
-import CalendarPage from './CalendarPage/CalendarPage';
+import SearchPage from "./SearchPage/SearchPage";
+import CalendarPage from "./CalendarPage/CalendarPage";
 
 // import 'react-dates/lib/css/_datepicker.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.pageWidth = React.createRef();
+  }
+
+  /**
+   * Calculate & Update state of new dimensions
+   */
+  updateDimensions() {
+    if (this.pageWidth.current.offsetWidth < 1200) {
+      this.pageWidth.current.classList.add("bu-smaller");
+    } else {
+    }
+  }
+
+  /**
+   * Add event listener
+   */
+  componentDidMount() {
+    this.updateDimensions();
+    // window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
   render() {
     const { portalCode, objectCode, locale, filters } = this.props;
 
     return (
-      <Query query={PORTAL_QUERY} variables={{ id: portalCode, locale }}>
-        {({ loading, error, data }) => {
-          if (loading)
-            return (
-              <div>
-                <Loading />
-              </div>
+      <div ref={this.pageWidth}>
+        <Query query={PORTAL_QUERY} variables={{ id: portalCode, locale }}>
+          {({ loading, error, data }) => {
+            if (loading)
+              return (
+                <div>
+                  <Loading />
+                </div>
+              );
+            if (error) return <div>Error</div>;
+
+            const PortalSite = data.PortalSite;
+            let options = data.PortalSite.options;
+
+            let root = document.documentElement;
+
+            root.style.setProperty(
+              "--bukazu-discount",
+              `${options.colors ? options.colors.discount : "orange"}`
             );
-          if (error) return <div>Error</div>;
-
-          const PortalSite = data.PortalSite;
-          let options = data.PortalSite.options;
-
-          let root = document.documentElement;
-
-          root.style.setProperty(
-            '--bukazu-discount',
-            `${options.colors ? options.colors.discount : 'orange'}`
-          );
-          root.style.setProperty(
-            '--bukazu-cell',
-            `${options.colors ? options.colors.cell : '#fff'}`
-          );
-          root.style.setProperty(
-            '--bukazu-arrival',
-            `${options.colors ? options.colors.arrival : '#6eeb83'}`
-          );
-          root.style.setProperty(
-            '--bukazu-booked',
-            `${options.colors ? options.colors.booked : '#ea2b1f'}`
-          );
-          root.style.setProperty(
-            '--bukazu-departure',
-            `${options.colors ? options.colors.departure : 'yellow'}`
-          );
-
-          root.style.setProperty(
-            '--bukazu-button',
-            `${
-              options.colors
-                ? options.colors.button
-                : 'rgba(23, 190, 187, 0.75)'
-            }`
-          );
-          root.style.setProperty(
-            '--bukazu-button_cta',
-            `${options.colors ? options.colors.buttonCta : '#e28413'}`
-          );
-          root.style.setProperty(
-            '--bukazu-background_month',
-            `${options.colors ? options.colors.month_background : '#e28413'}`
-          );
-          
-          if (objectCode && objectCode !== null) {
-            return (
-              <CalendarPage
-                PortalSite={PortalSite}
-                objectCode={objectCode}
-                locale={locale}
-              />
+            root.style.setProperty(
+              "--bukazu-cell",
+              `${options.colors ? options.colors.cell : "#fff"}`
             );
-          } else {
-            return (
-              <SearchPage
-                PortalSite={PortalSite}
-                locale={locale}
-                options={options}
-                filters={filters}
-              />
+            root.style.setProperty(
+              "--bukazu-arrival",
+              `${options.colors ? options.colors.arrival : "#6eeb83"}`
             );
-          }
-        }}
-      </Query>
+            root.style.setProperty(
+              "--bukazu-booked",
+              `${options.colors ? options.colors.booked : "#ea2b1f"}`
+            );
+            root.style.setProperty(
+              "--bukazu-departure",
+              `${options.colors ? options.colors.departure : "yellow"}`
+            );
+
+            root.style.setProperty(
+              "--bukazu-button",
+              `${
+                options.colors
+                  ? options.colors.button
+                  : "rgba(23, 190, 187, 0.75)"
+              }`
+            );
+            root.style.setProperty(
+              "--bukazu-button_cta",
+              `${options.colors ? options.colors.buttonCta : "#e28413"}`
+            );
+            root.style.setProperty(
+              "--bukazu-background_month",
+              `${options.colors ? options.colors.month_background : "#e28413"}`
+            );
+
+            if (objectCode && objectCode !== null) {
+              return (
+                <CalendarPage
+                  PortalSite={PortalSite}
+                  objectCode={objectCode}
+                  locale={locale}
+                />
+              );
+            } else {
+              return (
+                <SearchPage
+                  PortalSite={PortalSite}
+                  locale={locale}
+                  options={options}
+                  filters={filters}
+                />
+              );
+            }
+          }}
+        </Query>
+      </div>
     );
   }
 }
@@ -101,7 +127,7 @@ App.propTypes = {
   portalCode: PropTypes.string.isRequired,
   objectCode: PropTypes.string,
   locale: PropTypes.string.isRequired,
-  filters: PropTypes.object.isRequired,
+  filters: PropTypes.object.isRequired
 };
 
 export default App;
