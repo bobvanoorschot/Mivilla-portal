9b2313e50a50931da2eeb14df15fd9f14be76a23
Bob van Oorschot <bobvanoorschot@gmail.com>
2021-04-16 15:55:25 +0200

    Remove testing

--- src/components/__tests__/SafeBooking.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { mount } from '@cypress/react';
-import SafeBooking from '../SafeBooking';
-
-it('renders learn react link', () => {
-  mount(<SafeBooking />);
-  cy.get('a').contains('Learn React');
-});
