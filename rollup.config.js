d522b58c88274d2ce8ab2fa844688c6844e9a878
Bob van Oorschot <bobvanoorschot@gmail.com>
2021-08-21 22:15:16 +0200

    Fix css

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
-import pkg from './package.json'
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import postcssModules from 'postcss-modules';const cssExportMap = {};
 
 export default {
   input: 'src/index.js',
@@ -19,18 +17,7 @@ export default {
       exclude: "node_modules/**"
     }),
     postcss({
-      plugins: [
-        postcssModules({
-          getJSON (id, exportTokens) {
-            cssExportMap[id] = exportTokens;
-          }
-        })
-      ],
-      getExportNamed: false,
-      getExport (id) {
-        return cssExportMap[id];
-      },
-      extract: 'index.css',
+      extract: true,
     }),
     commonjs(),
     resolve(),
